3619708a97ef07d5e90d6e2a9b407eff851f4055
statsmaths <tbarnold@protonmail.ch>
2021-03-21 19:50:49 -0400

    auto update

--- dvt_workshop/funs.py
+++ dvt_workshop/funs.py
@@ -69,7 +69,7 @@ def dvt_embed_image(path, embed):
     from tensorflow.keras.preprocessing import image
     from tensorflow.keras.applications.vgg19 import preprocess_input
 
-    img = image.load_img(img_path, target_size=(224, 224))
+    img = image.load_img(path, target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
